1cd30df59f31692a5d3099e023da6bb296d51131
Artem <ahtyamof@gmail.com>
2022-10-30 19:00:36 +0300

    Initial Commit

---
+++ myHW_colorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myHW_colorizedApp
+//
+//  Created by Артём on 30.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
